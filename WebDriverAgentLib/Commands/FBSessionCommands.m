ac63a5fc2a231a181f03799f44dbdb878010a64d
jihaijun <jihaijun-os@360os.com>
2021-03-12 15:50:09 +0800

    update bundle id to com.tigerking.wda.runner

--- WebDriverAgentLib/Commands/FBSessionCommands.m
+++ WebDriverAgentLib/Commands/FBSessionCommands.m
@@ -179,7 +179,7 @@
 + (id<FBResponsePayload>)handleGetStatus:(FBRouteRequest *)request
 {
   // For updatedWDABundleId capability by Appium
-  NSString *productBundleIdentifier = @"com.facebook.WebDriverAgentRunner";
+  NSString *productBundleIdentifier = @"com.tigerking.wda.runner";
   NSString *envproductBundleIdentifier = NSProcessInfo.processInfo.environment[@"WDA_PRODUCT_BUNDLE_IDENTIFIER"];
   if (envproductBundleIdentifier && [envproductBundleIdentifier length] != 0) {
     productBundleIdentifier = NSProcessInfo.processInfo.environment[@"WDA_PRODUCT_BUNDLE_IDENTIFIER"];
